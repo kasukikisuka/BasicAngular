cc68a5c5271117919174db24d334dd96b06a3a8c
minatorak <hotmail22866@gmail.com>
2018-04-11 10:19:09 +0700

    make data binding html

--- src/app/widgets/widgets.component.ts
+++ src/app/widgets/widgets.component.ts
@@ -6,17 +6,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./widgets.component.css']
 })
 export class WidgetsComponent implements OnInit {
-  link = '/items';
-  color = 'blue';
+
+  widgets = [
+    {
+      id: 1,
+      name: 'Red Widget',
+      description: 'This is a red widget'
+    },
+    {
+      id: 2,
+      name: 'Orange Widget',
+      description: 'This is an orange widget'
+    },
+    {
+      id: 3,
+      name: 'Yellow Widget',
+      description: 'This is a yellow widget'
+    },
+    {
+      id: 4,
+      name: 'Green Widget',
+      description: 'This is a green widget'
+    },
+    {
+      id: 5,
+      name: 'Blue Widget',
+      description: 'This is a blue widget'
+    },
+    {
+      id: 6,
+      name: 'Indigo Widget',
+      description: 'This is a indigo widget'
+    },
+    {
+      id: 7,
+      name: 'Violet Widget',
+      description: 'This is a violet widget'
+    }
+  ]
 
   constructor() { }
 
   ngOnInit() {
 
-  }
-  echo(link) {
-    console.log('message', this.color);
-    console.log('message', link);
   }
 
 }
