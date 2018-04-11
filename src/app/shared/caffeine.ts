280700975de1da38f5d08d8bae1c03cc2772b640
minatorak <hotmail22866@gmail.com>
2018-04-11 18:49:42 +0700

    create CaffeineService mock data

---
+++ src/app/shared/caffeine.ts
@@ -0,0 +1,12 @@
+import {Injectable} from '@angular/core';
+
+@Injectable()
+export class CaffeineService {
+  sourse = [
+    'red blue',
+    'monster',
+    'rock star',
+    '4 loco',
+    '5 hour energy'
+  ]
+}
