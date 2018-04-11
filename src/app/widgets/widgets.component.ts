76026d459d92b7b6605cb88b7d6310d16f970397
minatorak <hotmail22866@gmail.com>
2018-04-11 10:49:54 +0700

    edit data

--- src/app/widgets/widgets.component.ts
+++ src/app/widgets/widgets.component.ts
@@ -6,8 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./widgets.component.css']
 })
 export class WidgetsComponent implements OnInit {
-
-  widgets = [
+    jSonfile = [
     {
       id: 1,
       name: 'Red Widget',
@@ -43,7 +42,47 @@ export class WidgetsComponent implements OnInit {
       name: 'Violet Widget',
       description: 'This is a violet widget'
     }
-  ]
+  ];
+
+  jsonfile1= [
+    {
+      id: 1,
+      name: 'Red Widget',
+      description: 'This is a red widget'
+    },
+    {
+      id: 2,
+      name: 'Orange Widget',
+      description: 'This is an orange widget'
+    },
+    {
+      id: 3,
+      name: 'Yellow Widget',
+      description: 'This is a yellow widget'
+    },
+    {
+      id: 4,
+      name: 'Green Widget',
+      description: 'This is a green widget'
+    },
+    {
+      id: 5,
+      name: 'Blue Widget',
+      description: 'This is a blue widget'
+    },
+    {
+      id: 6,
+      name: 'Indigo Widget',
+      description: 'This is a indigo widget'
+    },
+    {
+      id: 7,
+      name: 'Violet Widget',
+      description: 'This is a violet widget'
+    }
+  ];
+
+  JJSON = this.jsonfile1.concat(this.jSonfile);
 
   constructor() { }
 
